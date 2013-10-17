e817d433fe74280aba8b114195985a217d50720c
David Sankel <david@sankelsoftware.com>
2013-10-17 13:20:21 -0600

    Fixed formatting with smisc_unit.

--- include/smisc/unit.hpp
+++ include/smisc/unit.hpp
@@ -1,12 +1,10 @@
 #ifndef SMISC_UNIT_HPP_
 #define SMISC_UNIT_HPP_
 
-namespace smisc
-{
-    struct Unit
-    {
-        bool operator==( const Unit & ) const { return true; }
-    } const unit = Unit();
+namespace smisc {
+struct Unit {
+  bool operator==(const Unit&) const { return true; }
+} const unit = Unit();
 }
 
 #endif
