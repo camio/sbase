3448f064b46b7705fd037623be797e8815d22fb4
David Sankel <david@sankelsoftware.com>
2013-11-11 17:49:52 -0700

    Fixed formatting of sboost_fusionfoldr.

--- include/sboost/fusionfoldr.hpp
+++ include/sboost/fusionfoldr.hpp
@@ -8,70 +8,48 @@
 #include <boost/fusion/include/pop_back.hpp>
 #include <boost/fusion/include/back.hpp>
 
-namespace sboost
-{
-    namespace fusion
-    {
-        namespace result_of
-        {
-            template< typename Sequence, typename F >
-            struct foldr1
-            {
-                typedef typename boost::fusion::result_of::pop_front<Sequence const>::type
-                    Sequence2;
-                typedef typename boost::fusion::result_of::front<Sequence>::type State;
+namespace sboost {
+namespace fusion {
+namespace result_of {
+template <typename Sequence, typename F>
+struct foldr1 {
+  typedef typename boost::fusion::result_of::pop_front<Sequence const>::type
+      Sequence2;
+  typedef typename boost::fusion::result_of::front<Sequence>::type State;
 
-                typedef typename boost::fusion::result_of::fold< Sequence2
-                                                               , State
-                                                               , F
-                                                               >::type type;
-            };
-        }
+  typedef typename boost::fusion::result_of::fold<Sequence2, State, F>::type
+      type;
+};
+}
 
-        template< typename Sequence, typename F >
-        typename result_of::foldr1<Sequence,F>::type
-        foldr1( Sequence & seq
-              , F const & f
-              )
-        {
-            using namespace boost::fusion;
-            return fold( pop_front( seq )
-                       , front( seq )
-                       , f
-                       );
-        }
-    }
-    namespace fusion
-    {
-        namespace result_of
-        {
-            template< typename Sequence, typename F >
-            struct foldl1
-            {
-                typedef typename boost::fusion::result_of::pop_back<Sequence const>::type
-                    Sequence2;
-                typedef typename boost::fusion::result_of::back<Sequence>::type State;
+template <typename Sequence, typename F>
+typename result_of::foldr1<Sequence, F>::type foldr1(Sequence& seq,
+                                                     F const& f) {
+  using namespace boost::fusion;
+  return fold(pop_front(seq), front(seq), f);
+}
+}
+namespace fusion {
+namespace result_of {
+template <typename Sequence, typename F>
+struct foldl1 {
+  typedef typename boost::fusion::result_of::pop_back<Sequence const>::type
+      Sequence2;
+  typedef typename boost::fusion::result_of::back<Sequence>::type State;
 
-                typedef typename boost::fusion::result_of::reverse_fold< Sequence2
-                                    , State
-                                    , F
-                                    >::type type;
-            };
-        }
+  typedef typename boost::fusion::result_of::reverse_fold<Sequence2,
+                                                          State,
+                                                          F>::type type;
+};
+}
 
-        template< typename Sequence, typename F >
-        typename result_of::foldl1<Sequence,F>::type
-        foldl1( Sequence & seq
-              , F const & f
-              )
-        {
-            using namespace boost::fusion;
-            return reverse_fold( pop_back( seq )
-                               , back( seq )
-                               , f
-                               );
-        }
-    }
+template <typename Sequence, typename F>
+typename result_of::foldl1<Sequence, F>::type foldl1(Sequence& seq,
+                                                     F const& f) {
+  using namespace boost::fusion;
+  return reverse_fold(pop_back(seq), back(seq), f);
+}
+}
 }
 
 #endif
