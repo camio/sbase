750558c43ee5f44ec99a1791cdf367e265d05f19
David Sankel <david@sankelsoftware.com>
2014-04-11 15:05:21 -0600

    Documented and fixed semantics of foldr1 and foldl1 functions.
    Also added tests to verify the solution.
    
    The original semantics were erroneous in that they didn't match up with the
    Haskell version of these functions.

--- include/sboost/fusionfoldr.hpp
+++ include/sboost/fusionfoldr.hpp
@@ -10,14 +10,103 @@
 
 namespace sboost {
 
-// TODO: This inner namespace needs to be removed for our coding conventions.
-// Also, these functions should be moved into possibly a 'FusionFold1' namespace
-// class.
-namespace fusion {
+namespace result_of {
+
+// Return the type of the result of the 'foldr1' function assuming the
+// specified 'Sequence' is the type of its first argument and the specified
+// 'Function' is the type of its second argument.
+template <typename Sequence, typename F>
+struct foldr1;
+
+// Return the type of the result of the 'foldl1' function assuming the
+// specified 'Sequence' is the type of its first argument and the specified
+// 'Function' is the type of its second argument.
+template <typename Sequence, typename F>
+struct foldl1;
+}
+
+// Recursively call the specified 'binaryFunction' on the spencified
+// 'nonemptySequence' from right to left. If the sequence consists of just one
+// element, return that element. Otherwise return the result of the recursive
+// calls.
+//
+// Generally speaking if the 'nonemptySequence' is 'v₁, v₂, v₃…' and
+// 'binaryFunction' is 'f', this function will return '…f( v₁, f(v₂, v₃) )…'.
+template <typename Sequence, typename F>
+typename result_of::foldr1<Sequence, F>::type foldr1(Sequence& nonemptySequence,
+                                                     F const& binaryFunction);
+
+// Recursively call the specified 'binaryFunction' on the spencified
+// 'nonemptySequence' from left to right. If the sequence consists of just one
+// element, return that element. Otherwise return the result of the recursive
+// calls.
+//
+// Generally speaking if the 'nonemptySequence' is 'v₁, v₂, v₃…' and
+// 'binaryFunction' is 'f', this function will return '…f( f(v₁, v₂), v₃ )…'.
+template <typename Sequence, typename F>
+typename result_of::foldl1<Sequence, F>::type foldl1(Sequence& seq,
+                                                     F const& f);
+
+// ===========================================================================
+//                 INLINE DEFINITIONS
+// ===========================================================================
+
+namespace detail {
+template <typename BinaryFunction>
+struct Flipped {
+  // Create a new 'Flipped' object based on the specified 'binaryFunction'
+  explicit Flipped(BinaryFunction binaryFunction)
+      : m_binaryFunction(binaryFunction) {}
+
+  typedef Flipped<BinaryFunction> this_type;
+
+  template <typename T>
+  struct result;
+
+  template <typename FirstArg, typename SecondArg>
+  struct result<this_type(FirstArg, SecondArg)> {
+    typedef typename boost::result_of<BinaryFunction(SecondArg, FirstArg)>::type
+        type;
+  };
+
+  // Call the binary function this object corresponds to with the specified
+  // 'secondArg' as its first argument and 'firstArg' as its second argument.
+  template <typename FirstArg, typename SecondArg>
+  typename result<this_type(FirstArg, SecondArg)>::type operator()(
+      FirstArg firstArg,
+      SecondArg secondArg) const {
+    return m_binaryFunction(secondArg, firstArg);
+  }
+ private:
+  BinaryFunction m_binaryFunction;
+};
+}
+
 namespace result_of {
 template <typename Sequence, typename F>
 struct foldr1 {
-  typedef typename boost::fusion::result_of::pop_front<Sequence const>::type
+  typedef typename boost::fusion::result_of::pop_back<Sequence const>::type
+      Sequence2;
+  typedef typename boost::fusion::result_of::back<Sequence>::type State;
+
+  typedef typename boost::fusion::result_of::
+      reverse_fold<Sequence2, State, detail::Flipped<F>>::type type;
+};
+}
+
+
+template <typename Sequence, typename F>
+typename result_of::foldr1<Sequence, F>::type foldr1(Sequence& seq,
+                                                     F const& f) {
+  return boost::fusion::reverse_fold(boost::fusion::pop_back(seq),
+                                     boost::fusion::back(seq),
+                                     detail::Flipped<F>(f));
+}
+
+namespace result_of {
+template <typename Sequence, typename F>
+struct foldl1 {
+  typedef typename boost::fusion::result_of::pop_front<Sequence>::type
       Sequence2;
   typedef typename boost::fusion::result_of::front<Sequence>::type State;
 
@@ -27,34 +116,10 @@ struct foldr1 {
 }
 
 template <typename Sequence, typename F>
-typename result_of::foldr1<Sequence, F>::type foldr1(Sequence& seq,
-                                                     F const& f) {
-  using namespace boost::fusion;
-  return fold(pop_front(seq), front(seq), f);
-}
-}
-namespace fusion {
-namespace result_of {
-template <typename Sequence, typename F>
-struct foldl1 {
-  typedef typename boost::fusion::result_of::pop_back<Sequence const>::type
-      Sequence2;
-  typedef typename boost::fusion::result_of::back<Sequence>::type State;
-
-  typedef typename boost::fusion::result_of::reverse_fold<Sequence2,
-                                                          State,
-                                                          F>::type type;
-};
-}
-
-// TODO: I don't think the semantics of this function are what's normally
-// expected of a foldl1. This needs to be checked and possibly modified.
-template <typename Sequence, typename F>
 typename result_of::foldl1<Sequence, F>::type foldl1(Sequence& seq,
                                                      F const& f) {
-  using namespace boost::fusion;
-  return reverse_fold(pop_back(seq), back(seq), f);
-}
+  return boost::fusion::fold(
+      boost::fusion::pop_front(seq), boost::fusion::front(seq), f);
 }
 }
 
--- src/sboost_fusionfoldr.t.cpp
+++ src/sboost_fusionfoldr.t.cpp
@@ -2,16 +2,39 @@
 
 #include <boost/fusion/include/vector.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/make_shared.hpp>
 #include <sboost/fusionfoldr.hpp>
 #include <stest/testcollector.hpp>
 #include <string>
+#include <vector>
 
 namespace {
-struct StringConcat {
+struct StringConcatRight {
   typedef std::string result_type;
+  boost::shared_ptr<std::vector<std::pair<std::string,std::string> >> calls;
+
+  StringConcatRight()
+      : calls(boost::make_shared<
+            std::vector<std::pair<std::string, std::string>>>()) {}
+
+  template <typename T>
+  std::string operator()(T t, const std::string s) const {
+    calls->emplace_back( boost::lexical_cast<std::string>(t), s );
+    return boost::lexical_cast<std::string>(t) + s;
+  }
+};
+
+struct StringConcatLeft {
+  typedef std::string result_type;
+  boost::shared_ptr<std::vector<std::pair<std::string,std::string> >> calls;
+
+  StringConcatLeft()
+      : calls(boost::make_shared<
+            std::vector<std::pair<std::string, std::string>>>()) {}
 
   template <typename T>
   std::string operator()(const std::string s, T t) const {
+    calls->emplace_back( s, boost::lexical_cast<std::string>(t) );
     return s + boost::lexical_cast<std::string>(t);
   }
 };
@@ -22,14 +45,26 @@ void fusionfoldrTests(stest::TestCollector& col) {
   col.addTest("sboost_fusionfoldr_foldr1", []()->void {
     boost::fusion::vector<std::string, int, unsigned, std::string> v(
         "Hi", 3, 9u, "Ho");
-    BOOST_CHECK_EQUAL(sboost::fusion::foldr1(v, StringConcat()), "Hi39Ho");
+    StringConcatRight concat;
+    BOOST_CHECK_EQUAL(sboost::foldr1(v, concat), "Hi39Ho");
+    std::vector<std::pair<std::string, std::string>> expectedCalls;
+    expectedCalls.emplace_back("9", "Ho");
+    expectedCalls.emplace_back("3", "9Ho");
+    expectedCalls.emplace_back("Hi", "39Ho");
+    BOOST_CHECK(*concat.calls == expectedCalls);
   });
 
   col.addTest("sboost_fusionfoldr_foldl1", []()->void {
     boost::fusion::vector<std::string, int, unsigned, std::string> v(
         "Hi", 3, 9u, "Ho");
-    BOOST_CHECK_EQUAL(sboost::fusion::foldl1(v, StringConcat()),
-                      "Ho93Hi");
+    StringConcatLeft concat;
+    BOOST_CHECK_EQUAL(sboost::foldl1(v, concat), "Hi39Ho");
+
+    std::vector<std::pair<std::string, std::string>> expectedCalls;
+    expectedCalls.emplace_back("Hi", "3");
+    expectedCalls.emplace_back("Hi3", "9");
+    expectedCalls.emplace_back("Hi39", "Ho");
+    BOOST_CHECK(*concat.calls == expectedCalls);
   });
 }
 };
