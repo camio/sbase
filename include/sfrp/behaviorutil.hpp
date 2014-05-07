bc35d67431d8a042bcb60bf4fd27b794e38c9a27
David Sankel <david@sankelsoftware.com>
2014-05-07 17:40:24 -0600

    Added 'pure' and 'map' to sfrp_behaviorutil.

--- include/sfrp/behaviorutil.hpp
+++ include/sfrp/behaviorutil.hpp
@@ -2,6 +2,8 @@
 #define SFRP_BEHAVIORUTIL_HPP_
 
 #include <sfrp/behavior.hpp>
+#include <sfrp/behaviormap.hpp>
+#include <type_traits>
 
 namespace sfrp {
 // This class implements a namespace for simple utility operations on
@@ -13,7 +15,14 @@ struct BehaviorUtil {
   static Behavior<T> always(const T& value);
 
   // Return a behavior that has the value of the current time at every moment.
-  static Behavior< double > time( );
+  static Behavior<double> time();
+
+  // Return a behavior that, at any time 't', has a value of the specified
+  // 'timeFunction' applied to 't'. The behavior is undefined unless
+  // 'timeFunction' is defined for all input values.
+  template <typename Function>
+  static Behavior<typename std::result_of<Function(double)>::type> pure(
+      Function timeFunction);
 
   // Return a behavior with values equivelent to the specified 'valueBehavior',
   // but is undefined whenever the specified 'curtailingBehavior' is undefined.
@@ -25,6 +34,17 @@ struct BehaviorUtil {
   template <typename T, typename U>
   static Behavior<T> curtail(const Behavior<T>& valueBehavior,
                              const Behavior<U>& curtailingBehavior);
+
+  // Return a behavior that, for any time 't', is defined to be the specified
+  // 'function' applied to the specified 'argBehaviors' behaviors at time 't'.
+  // If any of 'argBehaviors' is not defined at time 't', then neither is the
+  // result behavior.
+  //
+  // The behavior is undefined unless the specified 'Function' meets the
+  // 'Deferred Callable Object' concept of Boost.Fusion.
+  template <typename Function, typename... ArgBehaviors>
+  static typename std::result_of<BehaviorMap(Function, ArgBehaviors...)>::type
+      map(Function function, ArgBehaviors... argBehaviors);
 };
 
 // ===========================================================================
@@ -37,16 +57,30 @@ Behavior<T> BehaviorUtil::always(const T& value) {
     return boost::make_optional(value);
   });
 }
+
 template <typename T, typename U>
 Behavior<T> BehaviorUtil::curtail(const Behavior<T>& valueBehavior,
-                                  const Behavior<U>& curtailingBehavior)
-{
+                                  const Behavior<U>& curtailingBehavior) {
   return Behavior<T>::fromValuePullFunc([valueBehavior, curtailingBehavior](
       double time) {
     boost::optional<T> value = valueBehavior.pull(time);
     boost::optional<U> curtail = curtailingBehavior.pull(time);
     return curtail ? value : boost::none;
   });
+}
+
+template <typename Function>
+static Behavior<typename std::result_of<Function(double)>::type>
+BehaviorUtil::pure(Function timeFunction) {
+  typedef typename std::result_of<Function(double)>::type ResultBehaviorValue;
+  return Behavior<ResultBehaviorValue>::fromValuePullFunc([timeFunction](
+      double time) { return boost::make_optional(timeFunction(time)); });
+}
+
+template <typename Function, typename... ArgBehaviors>
+typename std::result_of<BehaviorMap(Function, ArgBehaviors...)>::type
+BehaviorUtil::map(Function function, ArgBehaviors... argBehaviors) {
+  return sfrp::BehaviorMap()(function, argBehaviors...);
 }
 }
 #endif
