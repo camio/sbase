4400bfb65ee45846bd339d58076506659793a1cf
David Sankel <david@sankelsoftware.com>
2014-05-07 20:56:38 -0600

    sfrp_behaviortimeutil: doc/tests.

--- include/sfrp/behaviortimeutil.hpp
+++ include/sfrp/behaviortimeutil.hpp
@@ -1,12 +1,49 @@
 #ifndef SFRP_BEHAVIORTIMEUTIL_HPP_
 #define SFRP_BEHAVIORTIMEUTIL_HPP_
 
+//@PURPOSE: Provide behavior utility operations related to time.
+//
+//@CLASSES:
+//  sfrp::BehaviorTimeUtil: namespace for behavior time utility operations
+//
+//@DESCRIPTION: This component provides a single namespace class,
+// 'BehaviorTimeUtil', which includes behavior utility operations related to
+// time.
+//
+// Usage
+// -----
+// This section illustrates intended use of this component.
+//
+// Example 1: Derivative
+// - - - - - - - - - - -
+// With the two functions provided in this component, we can create a behavior
+// derivative function. The key thing to note is that a piece-wise derivative is
+// Δx/Δt. Each pull provides a different piece of the derivative.
+//
+// The argument to map below is the prior (value,time) pair paired with the
+// current (value,time) pair.
+//..
+//  sfrp::Behavior<double> derivative(sfrp::Behavior<double> behavior) {
+//    return sfrp::BehaviorUtil::map(
+//        [](std::pair<std::pair<double, double>, std::pair<double, double>>
+//               pairs) {
+//          return (pairs.second.first - pairs.first.first) /
+//                 (pairs.second.second - pairs.first.second);
+//        },
+//        sfrp::BehaviorTimeUtil::withPriorPull(
+//            std::make_pair(0.0, 0.0),
+//            sfrp::BehaviorTimeUtil::withTime(behavior)));
+//  }
+//..
+
 #include <sfrp/behavior.hpp>
 #include <sfrp/behaviorpairutil.hpp>
 #include <sfrp/behaviorutil.hpp>
 #include <utility>  // std::pair
 
 namespace sfrp {
+// This class implements a namespace for utility operations on
+// 'Behavior' objects relating to time.
 struct BehaviorTimeUtil {
 
   // Return a behavior equivelent to the specified 'behavior' except it is
--- src/sfrp_behaviortimeutil.t.cpp
+++ src/sfrp_behaviortimeutil.t.cpp
@@ -4,6 +4,18 @@
 #include <sfrp/triggerutil.hpp>
 #include <stest/testcollector.hpp>
 
+static sfrp::Behavior<double> derivative(sfrp::Behavior<double> behavior) {
+  return sfrp::BehaviorUtil::map(
+      [](std::pair<std::pair<double, double>, std::pair<double, double>>
+             pairs) {
+        return (pairs.second.first - pairs.first.first) /
+               (pairs.second.second - pairs.first.second);
+      },
+      sfrp::BehaviorTimeUtil::withPriorPull(
+          std::make_pair(0.0, 0.0),
+          sfrp::BehaviorTimeUtil::withTime(behavior)));
+}
+
 namespace sfrp {
 void behaviortimeutilTests(stest::TestCollector& col) {
   col.addTest("sfrp_behaviortimeutil_withPriorPull", []()->void {
@@ -16,6 +28,14 @@ void behaviortimeutilTests(stest::TestCollector& col) {
     BOOST_CHECK(b.pull(2.0) == std::make_pair(0, 1));
     p.second(2);
     BOOST_CHECK(b.pull(3.0) == std::make_pair(1, 2));
+  });
+  col.addTest("sfrp_behaviortimeutil_withTime", []()->void {
+    sfrp::Behavior<std::pair<int, double>> intBehaviorWithTime =
+        sfrp::BehaviorTimeUtil::withTime(sfrp::BehaviorUtil::always(3));
+
+    BOOST_CHECK(intBehaviorWithTime.pull(0.0) == std::make_pair(3, 0.0));
+    BOOST_CHECK(intBehaviorWithTime.pull(1.0) == std::make_pair(3, 1.0));
+    BOOST_CHECK(intBehaviorWithTime.pull(3.0) == std::make_pair(3, 3.0));
   });
 }
 }
