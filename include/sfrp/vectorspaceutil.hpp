9444d920454f81b4e8a0389e2f0d1efebadf6f0e
David Sankel <david@sankelsoftware.com>
2014-05-08 21:56:07 -0600

    sfrp_vectorspaceutil: doc/tests/compilation bug fixes.

--- include/sfrp/vectorspaceutil.hpp
+++ include/sfrp/vectorspaceutil.hpp
@@ -1,8 +1,43 @@
 #ifndef SFRP_VECTORSPACEUTIL_HPP_
 #define SFRP_VECTORSPACEUTIL_HPP_
 
+//@PURPOSE: Provide utility operations on vector space behaviors.
+//
+//@CLASSES:
+//  sfrp::VectorSpaceUtil: vector space behavior operations namespace
+//
+//@SEE_ALSO: sfrp_normedvectorspaceutil, smisc_vectorspace
+//
+//@DESCRIPTION: This component provides one namespace class, 'VectorSpaceUtil',
+// which provides integration and other vector space functionality.
+//
+// The 'integral()' function can be used to calculate the position of objects
+// based on preset velocities and accelerations. This is useful for gravity
+// simulation and other momentum operations.
+//
+// Usage
+// -----
+// This section illustrates intended use of this component.
+//
+// Example 1: Position Calculation
+// - - - - - - - - - - - -- - - -
+// Calculating the position of an object given an initial position, a velocity,
+// and an acceleration is a straightforward application of the physics
+// equations.
+//..
+//  static sfrp::Behavior<double> calculatePosition(
+//      double initialPosition,
+//      sfrp::Behavior<double> velocity,
+//      sfrp::Behavior<double> acceleration) {
+//    return sfrp::BehaviorUtil::always(initialPosition) +
+//           sfrp::VectorSpaceUtil::integral(
+//               velocity + sfrp::VectorSpaceUtil::integral(acceleration));
+//  }
+//..
+
 #include <sfrp/behavior.hpp>
 #include <sfrp/behaviormap.hpp>
+#include <sfrp/behaviortimeutil.hpp>
 #include <sfrp/wormhole.hpp>
 #include <smisc/point1d.hpp>
 #include <smisc/vectorspace.hpp>
@@ -32,37 +67,32 @@ struct VectorSpaceUtil {
 // ===========================================================================
 
 template <typename T>
-T VectorSpaceUtil_Sum(const T& a, const T& b) {
-  return a + b;
-}
-;
-template <typename T>
 Behavior<T> VectorSpaceUtil::sum(const Behavior<T>& v) {
   auto wh = sfrp::Wormhole<T>(smisc::zero<T>());
   return wh.setInputBehavior(
-      sfrp::BehaviorMap()(&VectorSpaceUtil_Sum<T>, wh.outputBehavior(), v));
+      sfrp::BehaviorMap()([](const T & a, const T & b) { return a + b; },
+                          wh.outputBehavior(),
+                          v));
 }
-
-template <typename V>
-struct VectorSpaceUtil_Integral {
-  typedef V result_type;
-
-  V operator()(
-      const std::pair<std::pair<V, double>, std::pair<V, double>> vs) const {
-    const double time = vs.second.second;
-    const double timePrev = vs.first.second;
-    const V v = vs.second.first;
-    // const V vPrev = vs.first.first;
-    // TODO: do something better here using vPrev
-    return (time - timePrev) * v;
-  }
-};
 
 template <typename T>
 Behavior<T> VectorSpaceUtil::integral(const Behavior<T>& v) {
   const Behavior<T> slices = sfrp::BehaviorMap()(
-      VectorSpaceUtil_Integral<T>(),
-      pmWithPrev(std::make_pair(smisc::zero<T>(), 0.0), pmWithTime(v)));
+      // Calculate the slice by computing the volume of the rectangle specified
+      // by the current value, the current time, and the previous time. At some
+      // point it may be interesting to make this more sophisiticated by using
+      // the previous value and computing the volume of a trapezoid instead of a
+      // square.
+          [](const std::pair<std::pair<T, double>, std::pair<T, double>> vs)
+              ->T const {
+            const double time = vs.second.second;
+            const double timePrev = vs.first.second;
+            const T v = vs.second.first;
+            return (time - timePrev) * v;
+          },
+          sfrp::BehaviorTimeUtil::withPriorPull(
+              std::make_pair(smisc::zero<T>(), 0.0),
+              sfrp::BehaviorTimeUtil::withTime(v)));
   return VectorSpaceUtil::sum(slices);
 }
 }
--- src/sfrp_vectorspaceutil.t.cpp
+++ src/sfrp_vectorspaceutil.t.cpp
@@ -1,9 +1,19 @@
 #include <sfrp/vectorspaceutil.t.hpp>
 
+#include <sfrp/behavioroperators.hpp>
 #include <sfrp/behaviorutil.hpp>
 #include <sfrp/vectorspaceutil.hpp>
 #include <smisc/point1dvectorspace.hpp>
 #include <stest/testcollector.hpp>
+
+static sfrp::Behavior<double> calculatePosition(
+    double initialPosition,
+    sfrp::Behavior<double> velocity,
+    sfrp::Behavior<double> acceleration) {
+  return sfrp::BehaviorUtil::always(initialPosition) +
+         sfrp::VectorSpaceUtil::integral(
+             velocity + sfrp::VectorSpaceUtil::integral(acceleration));
+}
 
 namespace sfrp {
 void vectorspaceutilTests(stest::TestCollector& col)
@@ -14,6 +24,15 @@ void vectorspaceutilTests(stest::TestCollector& col)
     BOOST_CHECK_EQUAL(*bSum.pull(0.0), 1.0);
     BOOST_CHECK_EQUAL(*bSum.pull(1.0), 2.0);
     BOOST_CHECK_EQUAL(*bSum.pull(2.0), 3.0);
+  });
+  col.addTest("sfrp_vectorspaceutil_integral", []()->void {
+    Behavior<double> b = sfrp::BehaviorUtil::pure([](double time) {
+      return time <= 1.0 ? 1.0 : 2.0;
+    });
+    Behavior<double> bIntegrated = sfrp::VectorSpaceUtil::integral(b);
+    BOOST_CHECK_EQUAL(bIntegrated.pull(0.0), 0.0);
+    BOOST_CHECK_EQUAL(bIntegrated.pull(1.0), 1.0);
+    BOOST_CHECK_EQUAL(bIntegrated.pull(2.0), 3.0);
   });
 }
 }
