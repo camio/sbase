ba95476444774310f8350df8017f253621344952
David Sankel <david@sankelsoftware.com>
2014-05-08 22:05:31 -0600

    sfrp_wormholeutil: docs.

--- include/sfrp/wormholeutil.hpp
+++ include/sfrp/wormholeutil.hpp
@@ -1,15 +1,43 @@
 #ifndef SFRP_WORMHOLEUTIL_HPP_
 #define SFRP_WORMHOLEUTIL_HPP_
 
-#include <sfrp/behavior.hpp>
+//@PURPOSE: Provide utility operations for 'Wormhole' objects.
+//
+//@CLASSES:
+//  sfrp::WormholeUtil: wormhole utility operation namespace
+//
+//@SEE_ALSO: sfrp_wormhole
+//
+//@DESCRIPTION: This component provides a single namespace class,
+// 'WormholeUtil', which includes utility operations for 'Wormhole' objects.
+//
+// The single function in this namespace is 'createWormhole' which is a
+// shorthand for the 'Wormhole' constructor because a type need not be
+// specified since it is deduced from the argument.
+//
+// Usage
+// -----
+// This section illustrates intended use of this component.
+//
+// Example 1: Create a 'double' wormhole
+// - - - - - - - - - - - - - - - - - - -
+// In this example we create a 'double' wormhole with an initial value of '0.0'.
+//..
+//  auto doubleWormhole = sfrp::WormholeUtil::createWormhole( 0.0 );
+//..
+// Note that the fact that this wormhole has type double is deduced based on the
+// argument of the 'createWormhole()' function.
+
 #include <sfrp/wormhole.hpp>
-#include <utility>  // std::pair
 
 namespace sfrp {
 
+// This class is a namespace for utility operations on 'Wormhole' objects.
 struct WormholeUtil {
+  // Create a new 'Wormhole' object with the specified 'initialValue' as its
+  // initial value.
   template <typename T>
-  static Wormhole<T> createWormhole(const T& t0);
+  static Wormhole<T> createWormhole(const T& initialValue);
 };
 
 // ===========================================================================
--- src/sfrp_wormholeutil.t.cpp
+++ src/sfrp_wormholeutil.t.cpp
@@ -1,6 +1,10 @@
 #include <sfrp/wormhole.hpp>
 #include <sfrp/wormholeutil.hpp>
 
+static void example() {
+  auto doubleWormhole = sfrp::WormholeUtil::createWormhole( 0.0 );
+  auto doubleWormhole2 = sfrp::Wormhole<double>( 0.0 );
+}
 static void compilationTest()
 {
   sfrp::Wormhole<int> w = sfrp::WormholeUtil::createWormhole( 33 );
